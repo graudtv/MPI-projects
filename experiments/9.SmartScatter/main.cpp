a4ecfb96e5a84860772935fe3307153c6f433980
graudtV <55806873+graudtV@users.noreply.github.com>
2022-03-14 07:42:09 +0300

    add SmartScatter

---
+++ experiments/9.SmartScatter/main.cpp
@@ -0,0 +1,31 @@
+#include "Support/OstreamHelpers.hpp"
+#include "cxxmpi/cxxmpi.hpp"
+#include <numeric>
+
+namespace mpi = cxxmpi;
+
+template <class Printable> auto printvec(const Printable &P) {
+  return util::wrapSquareBrackets(util::join(P, ", "));
+}
+
+constexpr size_t ArraySz = 40;
+
+int main(int argc, char *argv[]) {
+  mpi::MPIContext Ctx{&argc, &argv};
+
+  auto Rank = mpi::commRank();
+  constexpr int RootIdx = 0;
+
+  std::vector<int> SndBuf;
+  if (Rank == RootIdx) {
+    SndBuf.resize(ArraySz);
+    std::iota(SndBuf.begin(), SndBuf.end(), 0);
+    std::cout << mpi::whoami << ": [root]: sending data: " << printvec(SndBuf)
+              << std::endl;
+  }
+  auto RcvBuf = mpi::scatterFair(SndBuf, ArraySz, RootIdx);
+  std::cout << mpi::whoami << ": received data: " << printvec(RcvBuf)
+            << std::endl;
+
+  return 0;
+}
